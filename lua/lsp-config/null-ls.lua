06f1e1baef8191c08cd1bdfe9993ed4a22918b39
Virezox Zhu <virezox@gmail.com>
2022-08-06 15:13:32 +0800

    Add rustfmt

--- lua/lsp-config/null-ls.lua
+++ lua/lsp-config/null-ls.lua
@@ -6,6 +6,23 @@ local sources = {
   formatting.stylua.with({
     extra_args = { '--config-path', vim.fn.expand('~/.config/stylua.toml') },
   }),
+  formatting.rustfmt.with({
+    extra_args = function(params)
+      local Path = require('plenary.path')
+      local cargo_toml = Path:new(params.root .. '/' .. 'Cargo.toml')
+
+      if cargo_toml:exists() and cargo_toml:is_file() then
+        for _, line in ipairs(cargo_toml:readlines()) do
+          local edition = line:match([[^edition%s*=%s*%"(%d+)%"]])
+          if edition then
+            return { '--edition=' .. edition }
+          end
+        end
+      end
+      -- default edition when we don't find `Cargo.toml` or the `edition` in it.
+      return { '--edition=2021' }
+    end,
+  }),
 }
 
 null_ls.setup({
